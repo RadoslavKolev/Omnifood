f9a686739cd7102cc0dc13657d177ad40417530b
RadoslavKolev <radoslav.kolev02@gmail.com>
2022-12-15 11:51:36 +0200

    Applied Sticky navigation

--- js/script.js
+++ js/script.js
@@ -1,52 +1,65 @@
 //* Sets the current year to the footer
-const setCurrentYear = () => {
-  const yearEl = document.querySelector(".year");
-  const currentYear = new Date().getFullYear();
-  yearEl.textContent = currentYear;
-};
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
 
 //* Make mobile navigation work
-const openMobileNavBar = () => {
-  const headerEl = document.querySelector("header");
-  const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector("header");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
 
-  // "toggle" will add the class if it doesn't exist or vise versa
-  btnNavEl.addEventListener("click", () => {
-    headerEl.classList.toggle("nav-open");
-  });
-};
+// "toggle" will add the class if it doesn't exist or vise versa
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
 
 //* Implementing smooth scrolling animation
-const smoothScrolling = () => {
-  const headerEl = document.querySelector("header");
-  const allLinks = document.querySelectorAll("a:link");
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(link => link.addEventListener("click", (e) => {
+  e.preventDefault();
+  const href = link.getAttribute("href");
+
+  if (href === '#') {
+    // Scroll back to top
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  } 
   
-  allLinks.forEach(link => link.addEventListener("click", (e) => {
-    e.preventDefault();
-    const href = link.getAttribute("href");
+  // Scroll to other sections
+  if (href !== "#" && href.startsWith("#")) {
+    const sectionEl = document.querySelector(href);
+    sectionEl.scrollIntoView({
+      behavior: "smooth"
+    });
+  }
 
-    if (href === '#') {
-      // Scroll back to top
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-      });
-    } 
-    
-    // Scroll to other sections
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({
-        behavior: "smooth"
-      });
-    }
+  // Close mobile navigation
+  if (link.classList.contains("main-nav-link")) {
+    headerEl.classList.toggle("nav-open");
+  }
+}));
 
-    // Close mobile navigation
-    if (link.classList.contains("main-nav-link")) {
-      headerEl.classList.toggle("nav-open");
-    }
-  }));
-};
+//* Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(entries => {
+  const entry = entries[0];
+
+  if (!entry.isIntersecting) {
+    // If we add that class to the header, it makes the page "jump"
+    document.body.classList.add('sticky');
+  } else {
+    document.body.classList.remove('sticky');
+  }
+}, {
+  root: null, // In the viewport
+  threshold: 0, // It will fire when 0% of the Hero section is inside the viewport
+  rootMargin: '-80px' // It's overlapping the Featured in section
+});
+
+obs.observe(sectionHeroEl);
 
 //* Fixing flexbox gap property missing in some Safari versions
 const checkFlexGap = () => {
@@ -66,9 +79,6 @@ const checkFlexGap = () => {
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 
-setCurrentYear();
-openMobileNavBar();
-smoothScrolling();
 checkFlexGap();
 
 /*
