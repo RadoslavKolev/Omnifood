f8edb2f0896eab588851978c1462362579cb7ac0
RadoslavKolev <radoslav.kolev02@gmail.com>
2022-12-15 12:18:22 +0200

    Fixing Flexbox gap in Safari

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ const obs = new IntersectionObserver(entries => {
 
 obs.observe(sectionHeroEl);
 
-//* Fixing flexbox gap property missing in some Safari versions
+//* Fixing flexbox gap property missing in some Safari versions 
 const checkFlexGap = () => {
   const flex = document.createElement("div");
   flex.style.display = "flex";
@@ -80,54 +80,3 @@ const checkFlexGap = () => {
 }
 
 checkFlexGap();
-
-/*
-.no-flexbox-gap .main-nav-list li:not(:last-child) {
-  margin-right: 4.8rem;
-}
-
-.no-flexbox-gap .list-item:not(:last-child) {
-  margin-bottom: 1.6rem;
-}
-
-.no-flexbox-gap .list-icon:not(:last-child) {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .delivered-faces {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .meal-attribute:not(:last-child) {
-  margin-bottom: 2rem;
-}
-
-.no-flexbox-gap .meal-icon {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .footer-row div:not(:last-child) {
-  margin-right: 6.4rem;
-}
-
-.no-flexbox-gap .social-links li:not(:last-child) {
-  margin-right: 2.4rem;
-}
-
-.no-flexbox-gap .footer-nav li:not(:last-child) {
-  margin-bottom: 2.4rem;
-}
-
-@media (max-width: 75em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 3.2rem;
-  }
-}
-
-@media (max-width: 59em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 0;
-    margin-bottom: 4.8rem;
-  }
-}
-*/
